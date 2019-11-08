ed09e1636a57e0f384c6774e9217c70b6c7f8d8e
Pat Hickey <phickey@fastly.com>
2019-11-08 11:50:48 -0800

    move package output to `dist`, add gh action to upload it

--- deb_from_installation.sh
+++ deb_from_installation.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 set -ex
 if [ -n "$1" ]; then
-	export VERSION="$1"
+    OUTDIR=$1
+else
+    OUTDIR=$(PWD)/build
+fi
+
+if [ -n "$2" ]; then
+	export VERSION="$2"
 else
 	export VERSION=`./version.sh`
 fi
@@ -11,4 +17,4 @@ mkdir -p build/pkg/opt
 mkdir -p build/pkg/DEBIAN
 sed -e s/VERSION/$VERSION/ wasi-sdk.control > build/pkg/DEBIAN/control
 cp -R /opt/wasi-sdk build/pkg/opt/
-cd build && dpkg-deb -b pkg wasi-sdk_$VERSION\_amd64.deb
+cd build && dpkg-deb -b pkg $OUTDIR/wasi-sdk_$VERSION\_amd64.deb
